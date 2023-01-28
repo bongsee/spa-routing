f051999b14880e12e74d7fc831e5c12408e28530
bongsee <ajinsuk3@naver.com>
2023-01-28 19:49:46 +0900

    feat: Add index.js
    - main logic
      - 클릭한 링크의 경로값을 추출하여 해당 값에 따라 root요소의 내용을
        다르게 출력
      - 실제로 서버에 요청하는 것이 아닌 path값을 중간에 탈취하여 동적으로
        요소 생성 후 삽입하는 것이므로 새로고침 시 발생하는 서버 요청에 대한
        처리 로직도 포함(DOMContentLoaded 이벤트)

---
+++ 2_ajax/public/js/index.js
@@ -0,0 +1,27 @@
+import { Home, Service, About, NotFound} from './components.js'
+
+const navigationEl = document.getElementById('navigation');
+const rootEl = document.getElementById('root');
+
+const routes = [
+  {path:'/', component: Home},
+  {path:'/service', component: Service},
+  {path:'/about', component: About}
+]
+
+async function render(path) {
+  // path에 맞는 컴포넌트 반환
+  const component = routes.find((route) => route.path === path)?.component ?? NotFound;
+  // 만들어진 컴포넌트를 html root요소에 삽입
+  rootEl.replaceChildren(await component());
+}
+
+navigationEl.addEventListener('click', (event) => {
+  if(!event.target.matches('#navigation > li > a')) return;
+  event.preventDefault();
+
+  const path = event.target.getAttribute('href');
+  render(path)
+})
+
+window.addEventListener('DOMContentLoaded', () => render('/'));
